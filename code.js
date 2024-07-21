c7fd0fcf21185739742a597f0b4f23d3d40dbacf
Bigla361 <47911607+Bigla361@users.noreply.github.com>
2024-07-21 16:09:10 +1000

    Update code.js

--- code.js
+++ code.js
@@ -3,5 +3,5 @@ searchparams = new URLSearchParams(window.location.search)
 if (searchparams.has("code")) {
   text = document.createElement("p");
   text.innerText = "Auth Code: " + searchparams.get("code");
-  document.body.append(text);
+  document.getElementsByTagName("body")[0].append(text);
 }
