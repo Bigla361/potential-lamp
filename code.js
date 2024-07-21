c06f40ed06028329df541983cf6a405a8b7e5d41
Bigla361 <47911607+Bigla361@users.noreply.github.com>
2024-07-21 16:14:35 +1000

    Update code.js

--- code.js
+++ code.js
@@ -1,7 +1,9 @@
-searchparams = new URLSearchParams(window.location.search)
-
-if (searchparams.has("code")) {
-  text = document.createElement("p");
-  text.innerText = "Auth Code: " + searchparams.get("code");
-  document.getElementsByTagName("body")[0].append(text);
+function fun() {
+  searchparams = new URLSearchParams(window.location.search)
+  
+  if (searchparams.has("code")) {
+    text = document.createElement("p");
+    text.innerText = "Auth Code: " + searchparams.get("code");
+    document.getElementsByTagName("body")[0].append(text);
+  }
 }
